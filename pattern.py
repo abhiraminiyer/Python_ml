aa513b638d6cfa609c157959be7b40ede4e67503
abhiraminiyer <abhiraminiyer@gmail.com>
2023-04-25 17:42:49 +0530

    pattern

---
+++ pattern.py
@@ -0,0 +1,22 @@
+# for i in range(4):
+#     for j in range(3):
+#         print(1,end="\t")
+#     print()
+# for i in range(4):
+#     for j in range(3):
+#         print("*",end="\t")
+#     print()
+# for i in range(4):
+#     for j in range(3):
+#         print(i,end="\t")
+#     print()
+
+# for i in range(4):
+#     for j in range(3):
+#         print(j,end="\t")
+#     print()
+
+for i in range(4):
+    for j in range(3):
+        print(j+4,end="\t")
+    print()
